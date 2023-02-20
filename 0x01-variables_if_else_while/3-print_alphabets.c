2013a7099c2bf83f73c11b65214c276fa310394e
SoumayaZi <soumaya@example.com>
2023-02-20 15:19:13 -0800

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,10 +7,9 @@
 int main(void)
 {
 	char alph;
-	for (alph = 'a'; alph <= 'z'; alph++)
-	        { 
+
+	for (alph = 'a'; alph <= 'z'; alph++) 
 		putchar(alph);
-		}
 	for (alph = 'A'; alph <= 'Z'; alph++)
 		{
 		putchar(alph);
