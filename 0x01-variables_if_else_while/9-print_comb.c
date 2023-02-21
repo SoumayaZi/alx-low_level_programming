db0d1aa20ead478597fd283dc4d69a06c2f6e17e
SoumayaZi <soumaya@example.com>
2023-02-20 16:32:03 -0800

    printingspaced numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,8 @@ int main(void)
 	{
 		putchar(n);
 		if (n != '9')
-                { putchar(',');
-                  putchar(' ');
-		}
+                {putchar(',');
+                  putchar(' ');}
 	}
 		putchar('\n');
 
