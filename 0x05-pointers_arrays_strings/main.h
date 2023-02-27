353ec9240bbbc6310872e601c8ae3bef1e61261f
SoumayaZi <soumaya@example.com>
2023-02-27 15:57:17 -0800

    print rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _putchar(char c);
 size_t _strlen(const char *str);
 void print_rev(char *s);
-int _putchar(char c);
 #endif /* MAIN_H */
